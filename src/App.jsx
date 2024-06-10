769bcc08242e408f5e5f20bbb87a0f91bfe3c8b0
hebrahimnori <hebrahimnori@yahoo.com>
2024-06-11 01:22:11 +0330

    footer design

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import MainPage from "./pages/MainPage";
 import ProjectPage from "./pages/ProjectPage";
 import PhotoPage from "./pages/PhotoPage";
 import SkillPage from "./pages/SkillPage";
+import PageNotFound from "./pages/404";
+import ProjectsDetail from "./pages/ProjectsDetail";
+import Layout from "./layout/Layout";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/projects" element={<ProjectPage />} />
-      <Route path="/skills" element={<SkillPage />} />
-      <Route path="/photography" element={<PhotoPage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route index path="/" element={<MainPage />} replace />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/projects/:id" element={<ProjectsDetail />} />
+        <Route path="/skills" element={<SkillPage />} />
+        <Route path="/photography" element={<PhotoPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/layout/layout.jsx", "./src/components/Dock.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
